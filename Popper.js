8064270f42519be21c412a8cf4280b186145706e
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2024-07-26 14:54:13 -0400

    Create Popper.js

---
+++ Popper.js
@@ -0,0 +1,23 @@
+var q = document.createElement('div')
+q.style.margin = "0 auto"
+q.style.width = "84%"
+q.style.height = "1136px"
+q.style.z-index = "10px"
+q.style.border-radius = "8px"
+q.style.text-align = "center"
+q.style.box-sizing = "border-box"
+q.style.margin-top = "20px"
+q.style.position = "absolute"
+q.style.background = "white"
+
+var w = document.createElement('h3')
+w.style.margin = "20px 0 0 0"
+w.style.padding = "0"
+
+var e = document.createElement('p')
+e.style.margin = "15px"
+e.style.padding = "0"
+
+var r = document.createElement('a')
+r.style.color = "blue"
+r.style.line-height = "45px"
